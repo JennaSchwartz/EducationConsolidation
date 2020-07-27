0552908d273cc60e4628a4627b35ffa6125a1d93
sana-ajani <sajani6@gatech.edu>
2020-07-27 11:40:46 -0700

    test deployment

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Education Consolidation' });
+  res.render('index', { title: 'Education Consolidation!!' });
 });
 
 module.exports = router;
